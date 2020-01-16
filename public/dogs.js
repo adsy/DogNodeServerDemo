76aefd67542c4618e621910362f621310c706f50
Adam Brittain <adambrittain@Adams-MacBook-Pro.local>
2020-01-16 22:13:17 +1000

    first commit for project

---
+++ public/dogs.js
@@ -0,0 +1,25 @@
+
+
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+const doggos = document.querySelector(".doggos");
+
+function addNewDoggo() {
+  const promise = fetch(DOG_URL);
+  promise
+    .then(function(response) {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(function(processedResponse) {
+      const img = document.querySelector(".img");
+      const info = document.querySelector(".dog");
+      img.src = processedResponse.message;
+      img.alt = "Cute doggo";
+      info.innerText = "";
+      
+      console.log("test");
+    });
+}
+
+document.querySelector(".request-dog").addEventListener("click", addNewDoggo);
