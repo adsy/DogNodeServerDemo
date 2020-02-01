fda8771b17e985ed5817733378976ad3052795b4
Adam Brittain <adsy.britt@gmail.com>
2020-02-01 20:28:19 +1000

    Updated CRYPTO API for more reliable prices + added portfolio total - must add coing amounts in coins.js for total to appear correctly

--- public/coins.js
+++ public/coins.js
@@ -1,23 +1,46 @@
-const COIN_API = "https://coinlib.io/api/v1/coinlist?key=cf4bb8c07a196138&pref=AUD&page=1&order=volume_desc";
+//Use this proxy URL to avoid Same-Origin-Policy.
+var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 
-let coin_prices = new Array(3);
+const COIN_API = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=AUD&CMC_PRO_API_KEY=a9c6f35a-153a-4123-8709-7782164e2e2b";
 
-const COIN_promise = fetch(COIN_API);
+let coin_prices = new Array();
+
+const xtz_amt = 'XXX';
+const bat_amt = 'XXX';
+const link_amt = 'XXX';
+
+const COIN_promise = fetch(proxyUrl + COIN_API)
+
 COIN_promise.then(function (response) {
     const processingPromise = response.json();
     return processingPromise;
 })
     .then(function (processedResponse) {
-        for (let i = 0; i < processedResponse.coins.length; i++) {
-            if (processedResponse.coins[i].symbol === "XTZ") {
-                document.getElementById("xtz").textContent += "$" + parseFloat(processedResponse.coins[i].price).toFixed(2);
+        for (let i = 0; i < processedResponse.data.length; i++) {
+
+            if (processedResponse.data[i].symbol === "XTZ") {
+                document.getElementById("xtz").textContent += "$" + parseFloat(processedResponse.data[i].quote.AUD.price).toFixed(2);
+                coin_prices.push(parseFloat(processedResponse.data[i].quote.AUD.price).toFixed(2));
             }
-            else if (processedResponse.coins[i].symbol === "LINK") {
-                document.getElementById("link").textContent += "$" + parseFloat(processedResponse.coins[i].price).toFixed(2);
+            else if (processedResponse.data[i].symbol === "LINK") {
+                document.getElementById("link").textContent += "$" + parseFloat(processedResponse.data[i].quote.AUD.price).toFixed(2);
+                coin_prices.push(parseFloat(processedResponse.data[i].quote.AUD.price).toFixed(2));
             }
-            else if (processedResponse.coins[i].symbol === "BAT") {
-                document.getElementById("bat").textContent += "$" + parseFloat(processedResponse.coins[i].price).toFixed(2);
+            else if (processedResponse.data[i].symbol === "BAT") {
+                document.getElementById("bat").textContent += "$" + parseFloat(processedResponse.data[i].quote.AUD.price).toFixed(2);
+                coin_prices.push(parseFloat(processedResponse.data[i].quote.AUD.price).toFixed(2));
             }
         }
+
+        for (let i = 0; i < coin_prices.length; i++) {
+            console.log(coin_prices[i]);
         }
+
+
+        document.getElementById("total").textContent += "$" + parseFloat((coin_prices[0] * xtz_amt) + (coin_prices[1] * link_amt) + (coin_prices[2] * bat_amt)).toFixed(2);
+
+
+    }
     );
+
+
--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.get("/", function(req, res) {
+
+const request = require('request');
+
+app.get("/", function(req, res, next) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.use("/public", express.static("./public"));
 
+
 app.listen(3000);
-console.log("listening on http://localhost:3000");
+
